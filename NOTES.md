7d9a32d1f5b65ac1a4672d97d2b983e33dd4ca37
Daniel Wojda <wojdadan@gmail.com>
2020-01-15 22:10:41 +0000

    Counter Actor example

--- NOTES.md
+++ NOTES.md
@@ -1,3 +1,11 @@
+# Notes
 
+* The most important page: [Akka actor typed docs](https://doc.akka.io/docs/akka/current/typed/guide/index.html)
+* The implementation of the actor is in the same file as test so you can focus on one file
 
-[Akka actor typed docs](https://doc.akka.io/docs/akka/current/typed/guide/index.html)
+You can think about `Behaviour[T]` as `T => Next Behavior` (simplification)
+
+Behaviors provides many useful factory methods, for instance:
+* Behaviors.same
+* Behaviors.receiveMessage
+* Behaviors.logMessages
--- src/test/scala/org/danielwojda/playground/_1/CounterSpec.scala
+++ src/test/scala/org/danielwojda/playground/_1/CounterSpec.scala
@@ -1,11 +1,53 @@
 package org.danielwojda.playground._1
 
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
+import akka.util.Timeout
+import org.scalatest.concurrent.{Futures, ScalaFutures}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.must.Matchers
 
-class CounterSpec extends AnyFlatSpec with Matchers {
+import scala.concurrent.{ExecutionContextExecutor, Future}
+import scala.concurrent.duration._
 
-  "Counter Actor" must "count commands" in {
-    1 mustBe 1
+class CounterSpec extends AnyFlatSpec with Matchers with ScalaFutures {
+
+  "Counter Actor" must "know know how to count" in {
+    val system = ActorSystem(CounterActor(), name = "Counter")
+    val actor: ActorRef[CounterActor.Command] = system
+    implicit val timeout: Timeout = Timeout(3.seconds)
+    implicit val scheduler: Scheduler = system.scheduler
+    implicit val ec: ExecutionContextExecutor = system.executionContext
+
+    actor ! CounterActor.Inc
+    actor ! CounterActor.Inc
+    actor.tell(CounterActor.Inc)
+    actor ! CounterActor.Inc
+
+    import akka.actor.typed.scaladsl.AskPattern._
+    val counterFuture: Future[CounterActor.Counter] =
+      actor.ask[CounterActor.Counter](CounterActor.Get)(timeout, scheduler)
+
+    whenReady(counterFuture){ counter =>
+      counter.value mustBe 4
+    }
+  }
+}
+
+object CounterActor {
+  //Protocol
+  sealed trait Command
+  final case object Inc extends Command
+  final case class Counter(value: Long) // used as a response only
+  final case class Get(replyTo: ActorRef[Counter]) extends Command //Returned by the actor, must contain the replay to actor ref
+
+  def apply(): Behavior[CounterActor.Command] = counterBehaviour(0)
+
+  private def counterBehaviour(value: Long): Behaviors.Receive[Command] = Behaviors.receiveMessage {
+    case Inc =>
+      counterBehaviour(value + 1)
+    case Get(replyTo) =>
+      replyTo ! Counter(value)
+      Behaviors.same
   }
 }
