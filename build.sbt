a01727a120a8c9fcc7529f02e3272a7d8df74859
Daniel Wojda <wojdadan@gmail.com>
2020-01-15 23:15:53 +0000

    Akka testkit example

--- build.sbt
+++ build.sbt
@@ -5,4 +5,5 @@ version := "0.1"
 scalaVersion := "2.12.10"
 
 libraryDependencies += "com.typesafe.akka" %% "akka-actor-typed" % "2.6.1"
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.1.0" % Test
+libraryDependencies += "com.typesafe.akka" %% "akka-actor-testkit-typed" % "2.6.1" % Test
+libraryDependencies += "org.scalatest" %% "scalatest" % "3.0.8" % Test
--- src/test/scala/org/danielwojda/playground/_1/CounterSpec.scala
+++ src/test/scala/org/danielwojda/playground/_1/CounterSpec.scala
@@ -1,45 +1,55 @@
 package org.danielwojda.playground._1
 
+import akka.actor.testkit.typed.scaladsl.ScalaTestWithActorTestKit
 import akka.actor.typed.scaladsl.Behaviors
 import akka.actor.typed.{ActorRef, ActorSystem, Behavior, Scheduler}
 import akka.util.Timeout
-import org.scalatest.concurrent.{Futures, ScalaFutures}
-import org.scalatest.flatspec.AnyFlatSpec
-import org.scalatest.matchers.must.Matchers
+import org.scalatest.FlatSpecLike
 
-import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.concurrent.duration._
+import scala.concurrent.{ExecutionContextExecutor, Future}
 
-class CounterSpec extends AnyFlatSpec with Matchers with ScalaFutures {
+class CounterSpec extends ScalaTestWithActorTestKit with FlatSpecLike {
 
-  "Counter Actor" must "know know how to count" in {
-    val system = ActorSystem(CounterActor(), name = "Counter")
+  "Counter Actor" must "know how to count" in {
+    val system = ActorSystem(guardianBehavior = CounterActor(), name = "Counter") // it should be stopped at the end of test
     val actor: ActorRef[CounterActor.Command] = system
-    implicit val timeout: Timeout = Timeout(3.seconds)
-    implicit val scheduler: Scheduler = system.scheduler
+    val timeout: Timeout = Timeout(3.seconds)
+    val scheduler: Scheduler = system.scheduler
     implicit val ec: ExecutionContextExecutor = system.executionContext
 
+    actor ! CounterActor.Inc      // Fire and forget
     actor ! CounterActor.Inc
-    actor ! CounterActor.Inc
-    actor.tell(CounterActor.Inc)
+    actor.tell(CounterActor.Inc)  // the same as !
     actor ! CounterActor.Inc
 
     import akka.actor.typed.scaladsl.AskPattern._
     val counterFuture: Future[CounterActor.Counter] =
-      actor.ask[CounterActor.Counter](CounterActor.Get)(timeout, scheduler)
+      actor.ask[CounterActor.Counter](CounterActor.Get)(timeout, scheduler) // Get the answer from the actor
 
     whenReady(counterFuture){ counter =>
-      counter.value mustBe 4
+      counter.value shouldBe 4
     }
+  }
+
+  it must "know how to count - tested with akka testkit" in {
+    val actor = testKit.spawn(CounterActor(), "counter")
+    val probe = testKit.createTestProbe[CounterActor.Counter]()
+
+    actor ! CounterActor.Inc
+    actor ! CounterActor.Inc
+    actor ! CounterActor.Get(probe.ref)
+
+    probe.expectMessage(CounterActor.Counter(2))
   }
 }
 
 object CounterActor {
   //Protocol
-  sealed trait Command
+  sealed trait Command //root type of all incoming messages
   final case object Inc extends Command
   final case class Counter(value: Long) // used as a response only
-  final case class Get(replyTo: ActorRef[Counter]) extends Command //Returned by the actor, must contain the replay to actor ref
+  final case class Get(replyTo: ActorRef[Counter]) extends Command
 
   def apply(): Behavior[CounterActor.Command] = counterBehaviour(0)
 
